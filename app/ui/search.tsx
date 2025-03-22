a1b3df6ea4baaf6306f01e880bf8c5564e65f463
jaimetellezb <jaimetellez410@gmail.com>
2025-03-22 14:03:09 -0500

    feature: changes chapter 15

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,32 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce'; 
+// lib para utilizar debouncing, que permite ejecutar el handleSearch 
+// después de que pase un determinado tiempo sin pulsar una tecla, en este caso 300 ms
+// al usar esta técnica puede reducir el número de solicitudes enviadas a su base de datos, ahorrando así recursos.
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+    // se utiliza para convertir lo ingresado en el campo de texto en un search params
+    // y lo concatena al final en la URL como ?query=<lo escrito>
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    // reemplazar la URL con useRouter y sus params, con lo que escribe el usuario
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +35,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
